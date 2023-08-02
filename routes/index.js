6c713638193077c87aa17f8b7c059f0c4c3c8926
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-08-03 00:43:56 +0330

    create and showall API for template model

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const apiRoutes = require('./api');
+const categoryRoutes = require('./categoryRoutes');
+
 
 router.use('/api', apiRoutes);
+router.use('/category', categoryRoutes);
+
 
 
 module.exports = router;
