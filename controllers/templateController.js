93a20991cd89f1c7ce941e6bb36c435290ef474b
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2023-08-03 01:11:57 +0330

    template edit api completed

--- controllers/templateController.js
+++ controllers/templateController.js
@@ -35,13 +35,60 @@ exports.showAllTemplates = async (req, res) => {
     }
 };
 
-// exports.editTemplate = async (req, res) => {
-//     try {
-      
-//     } catch (error) {
-//       res.status(500).json({ error: 'Error creating Template.' });
-//     }
-// };
+
+exports.editTemplate = async (req, res) => {
+  try {
+    const templateId = req.params.id;
+    const { title, description, categoryName } = req.body;
+
+    // Find the template by ID
+    const template = await Template.findByPk(templateId);
+
+    if (!template) {
+      return res.status(404).json({ error: "Template not found." });
+    }
+
+    // Find or create the category by name
+    let category;
+    if (categoryName) {
+      category = await Category.findOne({
+        where: {
+          name: categoryName
+        }
+      });
+
+      if (!category) {
+        // Create the category if it doesn't exist
+        try{
+          category = await Category.create({ name: categoryName });
+        }catch(error){
+          return res.status(500).json({ error: 'Error editing template. Can not create Category in which did not exist!' });
+        }
+      }
+    }
+
+    // Update the template's properties based on provided values
+    const updatedFields = {};
+    if (title) {
+      updatedFields.title = title;
+    }
+    if (description) {
+      updatedFields.description = description;
+    }
+    await template.update(updatedFields);
+
+    // Associate the template with the category if category is provided
+    if (category) {
+      await template.setCategory(category);
+    }
+
+    res.status(200).json(template);
+  } catch (error) {
+    console.error('Error editing template:', error);
+    res.status(500).json({ error: 'Error editing template.' });
+  }
+};
+
 
 // exports.deleteTemplate = async (req, res) => {
 //     try {
--- routes/templateRoutes.js
+++ routes/templateRoutes.js
@@ -6,6 +6,6 @@ const templateController = require('../controllers/templateController');
 
 templateRouter.post('/create', templateController.createTemplate)
 templateRouter.get('/showall', templateController.showAllTemplates)
-
+templateRouter.put('/:id', templateController.editTemplate)
 
 module.exports = templateRouter;
